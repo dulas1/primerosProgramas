81fa12e8631dc6679a796ce2f89d29ce14d67076
dulas1 <ti13110192@itess.edu.mx>
2017-08-05 14:12:18 -0500

    Agrega Numeros los imprime y Ordena de menor a mayor

--- Numeros/Numeros/Form1.Designer.vb
+++ Numeros/Numeros/Form1.Designer.vb
@@ -22,9 +22,63 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.lb1 = New System.Windows.Forms.ListBox()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btn1
+        '
+        Me.btn1.Location = New System.Drawing.Point(24, 12)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(160, 23)
+        Me.btn1.TabIndex = 1
+        Me.btn1.Text = "Agregar Numeros"
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'btn2
+        '
+        Me.btn2.Location = New System.Drawing.Point(24, 41)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(160, 23)
+        Me.btn2.TabIndex = 2
+        Me.btn2.Text = "Mostrar"
+        Me.btn2.UseVisualStyleBackColor = True
+        '
+        'lb1
+        '
+        Me.lb1.FormattingEnabled = True
+        Me.lb1.Location = New System.Drawing.Point(13, 97)
+        Me.lb1.Name = "lb1"
+        Me.lb1.Size = New System.Drawing.Size(182, 82)
+        Me.lb1.TabIndex = 3
+        '
+        'btn3
+        '
+        Me.btn3.Location = New System.Drawing.Point(24, 71)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(160, 23)
+        Me.btn3.TabIndex = 4
+        Me.btn3.Text = "Ordenar"
+        Me.btn3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(211, 197)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.lb1)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
-    End Sub
+        Me.ResumeLayout(False)
 
+    End Sub
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents lb1 As ListBox
+    Friend WithEvents btn3 As Button
 End Class
--- Numeros/Numeros/Form1.vb
+++ Numeros/Numeros/Form1.vb
@@ -1,3 +1,28 @@
 ﻿Public Class Form1
 
+    Dim numeros() As Integer
+    Dim num As Integer
+
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        For t = 0 To 4
+            ReDim Preserve numeros(t)
+            numeros(t) = InputBox("agrega un numero")
+        Next
+
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+
+        For I As Byte = 0 To 4
+            lb1.Items.Add(numeros(I))
+        Next
+        lb1.Items.Add(" ")
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        Array.Sort(numeros)
+        For Each nume In numeros
+            lb1.Items.Add(nume)
+        Next
+    End Sub
 End Class
