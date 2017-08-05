aae051d1ae97504c99fb4a14945987fb26a447f5
dulas1 <ti13110192@itess.edu.mx>
2017-08-05 11:57:48 -0500

    calculadora lista

--- Calculadora/Calculadora/Form1.Designer.vb
+++ Calculadora/Calculadora/Form1.Designer.vb
@@ -22,9 +22,237 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btnigual = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btnmenos = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btnpor = New System.Windows.Forms.Button()
+        Me.btnpunto = New System.Windows.Forms.Button()
+        Me.btn0 = New System.Windows.Forms.Button()
+        Me.btnentre = New System.Windows.Forms.Button()
+        Me.btnmas = New System.Windows.Forms.Button()
+        Me.txt1 = New System.Windows.Forms.TextBox()
+        Me.btnlimpiar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btn1
+        '
+        Me.btn1.Location = New System.Drawing.Point(12, 125)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(38, 37)
+        Me.btn1.TabIndex = 1
+        Me.btn1.Text = "1"
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'btn2
+        '
+        Me.btn2.Location = New System.Drawing.Point(56, 125)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(38, 37)
+        Me.btn2.TabIndex = 2
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = True
+        '
+        'btn3
+        '
+        Me.btn3.Location = New System.Drawing.Point(100, 125)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(38, 37)
+        Me.btn3.TabIndex = 3
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = True
+        '
+        'btnigual
+        '
+        Me.btnigual.BackColor = System.Drawing.Color.LightCoral
+        Me.btnigual.Location = New System.Drawing.Point(100, 168)
+        Me.btnigual.Name = "btnigual"
+        Me.btnigual.Size = New System.Drawing.Size(38, 37)
+        Me.btnigual.TabIndex = 4
+        Me.btnigual.Text = "="
+        Me.btnigual.UseVisualStyleBackColor = False
+        '
+        'btn4
+        '
+        Me.btn4.Location = New System.Drawing.Point(12, 82)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(38, 37)
+        Me.btn4.TabIndex = 5
+        Me.btn4.Text = "4"
+        Me.btn4.UseVisualStyleBackColor = True
+        '
+        'btn5
+        '
+        Me.btn5.Location = New System.Drawing.Point(56, 82)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(38, 37)
+        Me.btn5.TabIndex = 6
+        Me.btn5.Text = "5"
+        Me.btn5.UseVisualStyleBackColor = True
+        '
+        'btn6
+        '
+        Me.btn6.Location = New System.Drawing.Point(100, 82)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(38, 37)
+        Me.btn6.TabIndex = 7
+        Me.btn6.Text = "6"
+        Me.btn6.UseVisualStyleBackColor = True
+        '
+        'btnmenos
+        '
+        Me.btnmenos.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.btnmenos.Location = New System.Drawing.Point(144, 125)
+        Me.btnmenos.Name = "btnmenos"
+        Me.btnmenos.Size = New System.Drawing.Size(38, 37)
+        Me.btnmenos.TabIndex = 8
+        Me.btnmenos.Text = "-"
+        Me.btnmenos.UseVisualStyleBackColor = False
+        '
+        'btn7
+        '
+        Me.btn7.Location = New System.Drawing.Point(12, 39)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(38, 37)
+        Me.btn7.TabIndex = 9
+        Me.btn7.Text = "7"
+        Me.btn7.UseVisualStyleBackColor = True
+        '
+        'btn8
+        '
+        Me.btn8.Location = New System.Drawing.Point(56, 39)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(38, 37)
+        Me.btn8.TabIndex = 10
+        Me.btn8.Text = "8"
+        Me.btn8.UseVisualStyleBackColor = True
+        '
+        'btn9
+        '
+        Me.btn9.Location = New System.Drawing.Point(100, 39)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(38, 37)
+        Me.btn9.TabIndex = 11
+        Me.btn9.Text = "9"
+        Me.btn9.UseVisualStyleBackColor = True
+        '
+        'btnpor
+        '
+        Me.btnpor.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.btnpor.Location = New System.Drawing.Point(144, 82)
+        Me.btnpor.Name = "btnpor"
+        Me.btnpor.Size = New System.Drawing.Size(38, 37)
+        Me.btnpor.TabIndex = 12
+        Me.btnpor.Text = "*"
+        Me.btnpor.UseVisualStyleBackColor = False
+        '
+        'btnpunto
+        '
+        Me.btnpunto.Location = New System.Drawing.Point(12, 168)
+        Me.btnpunto.Name = "btnpunto"
+        Me.btnpunto.Size = New System.Drawing.Size(38, 37)
+        Me.btnpunto.TabIndex = 13
+        Me.btnpunto.Text = "."
+        Me.btnpunto.UseVisualStyleBackColor = True
+        '
+        'btn0
+        '
+        Me.btn0.Location = New System.Drawing.Point(56, 168)
+        Me.btn0.Name = "btn0"
+        Me.btn0.Size = New System.Drawing.Size(38, 37)
+        Me.btn0.TabIndex = 14
+        Me.btn0.Text = "0"
+        Me.btn0.UseVisualStyleBackColor = True
+        '
+        'btnentre
+        '
+        Me.btnentre.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.btnentre.Location = New System.Drawing.Point(144, 39)
+        Me.btnentre.Name = "btnentre"
+        Me.btnentre.Size = New System.Drawing.Size(38, 37)
+        Me.btnentre.TabIndex = 16
+        Me.btnentre.Text = "/"
+        Me.btnentre.UseVisualStyleBackColor = False
+        '
+        'btnmas
+        '
+        Me.btnmas.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.btnmas.Location = New System.Drawing.Point(144, 168)
+        Me.btnmas.Name = "btnmas"
+        Me.btnmas.Size = New System.Drawing.Size(38, 37)
+        Me.btnmas.TabIndex = 15
+        Me.btnmas.Text = "+"
+        Me.btnmas.UseVisualStyleBackColor = False
+        '
+        'txt1
+        '
+        Me.txt1.Enabled = False
+        Me.txt1.Location = New System.Drawing.Point(13, 13)
+        Me.txt1.Name = "txt1"
+        Me.txt1.Size = New System.Drawing.Size(101, 20)
+        Me.txt1.TabIndex = 17
+        '
+        'btnlimpiar
+        '
+        Me.btnlimpiar.Location = New System.Drawing.Point(119, 12)
+        Me.btnlimpiar.Name = "btnlimpiar"
+        Me.btnlimpiar.Size = New System.Drawing.Size(62, 22)
+        Me.btnlimpiar.TabIndex = 18
+        Me.btnlimpiar.Text = "limpiar"
+        Me.btnlimpiar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
-    End Sub
+        Me.ClientSize = New System.Drawing.Size(193, 214)
+        Me.Controls.Add(Me.btnlimpiar)
+        Me.Controls.Add(Me.txt1)
+        Me.Controls.Add(Me.btnentre)
+        Me.Controls.Add(Me.btnmas)
+        Me.Controls.Add(Me.btn0)
+        Me.Controls.Add(Me.btnpunto)
+        Me.Controls.Add(Me.btnpor)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btn7)
+        Me.Controls.Add(Me.btnmenos)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btnigual)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn1)
+        Me.Name = "Form1"
+        Me.Text = "Calculadora"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
 
+    End Sub
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btnigual As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btnmenos As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btnpor As Button
+    Friend WithEvents btnpunto As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents btnentre As Button
+    Friend WithEvents btnmas As Button
+    Friend WithEvents txt1 As TextBox
+    Friend WithEvents btnlimpiar As Button
 End Class
--- Calculadora/Calculadora/Form1.vb
+++ Calculadora/Calculadora/Form1.vb
@@ -1,3 +1,91 @@
 ﻿Public Class Form1
 
+    Dim A As Double
+    Dim B As Double
+    Dim OPERACION As String
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        txt1.Text = txt1.Text & "1"
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        txt1.Text = txt1.Text & "2"
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        txt1.Text = txt1.Text & "3"
+    End Sub
+
+    Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        txt1.Text = txt1.Text & "4"
+    End Sub
+
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        txt1.Text = txt1.Text & "5"
+    End Sub
+
+    Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        txt1.Text = txt1.Text & "6"
+    End Sub
+
+    Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        txt1.Text = txt1.Text & "7"
+    End Sub
+
+    Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        txt1.Text = txt1.Text & "8"
+    End Sub
+
+    Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        txt1.Text = txt1.Text & "9"
+    End Sub
+
+    Private Sub btnpunto_Click(sender As Object, e As EventArgs) Handles btnpunto.Click
+        txt1.Text = txt1.Text & "."
+    End Sub
+
+    Private Sub btn0_Click(sender As Object, e As EventArgs) Handles btn0.Click
+        txt1.Text = txt1.Text & "0"
+    End Sub
+
+    Private Sub btnigual_Click(sender As Object, e As EventArgs) Handles btnigual.Click
+        B = txt1.Text
+        txt1.Text = ""
+        If OPERACION = "+" Then
+            txt1.Text = A + B
+        ElseIf OPERACION = "-" Then
+            txt1.Text = A - B
+        ElseIf OPERACION = "*" Then
+            txt1.Text = A * B
+        ElseIf OPERACION = "/" Then
+            txt1.Text = A / B
+        End If
+    End Sub
+
+    Private Sub btnmas_Click(sender As Object, e As EventArgs) Handles btnmas.Click
+        A = txt1.Text
+        txt1.Text = ""
+        OPERACION = "+"
+    End Sub
+
+    Private Sub btnmenos_Click(sender As Object, e As EventArgs) Handles btnmenos.Click
+        A = txt1.Text
+        txt1.Text = ""
+        OPERACION = "-"
+    End Sub
+
+    Private Sub btnpor_Click(sender As Object, e As EventArgs) Handles btnpor.Click
+        A = txt1.Text
+        txt1.Text = ""
+        OPERACION = "*"
+    End Sub
+
+    Private Sub btnentre_Click(sender As Object, e As EventArgs) Handles btnentre.Click
+        A = txt1.Text
+        txt1.Text = ""
+        OPERACION = "/"
+    End Sub
+
+    Private Sub btnlimpiar_Click(sender As Object, e As EventArgs) Handles btnlimpiar.Click
+        txt1.Text = ""
+    End Sub
 End Class
