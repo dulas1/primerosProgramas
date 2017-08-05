98196617aa0b002f9a2ac563bb5de5d9ffc87b4d
dulas1 <ti13110192@itess.edu.mx>
2017-08-05 12:34:52 -0500

    Agrega el nombre y apellido en una lista de texto

--- Nombre/Nombre/Form1.Designer.vb
+++ Nombre/Nombre/Form1.Designer.vb
@@ -22,9 +22,85 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txt1 = New System.Windows.Forms.TextBox()
+        Me.txt2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.lbox1 = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(14, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nombre(s)"
+        '
+        'txt1
+        '
+        Me.txt1.Location = New System.Drawing.Point(75, 13)
+        Me.txt1.Name = "txt1"
+        Me.txt1.Size = New System.Drawing.Size(134, 20)
+        Me.txt1.TabIndex = 1
+        '
+        'txt2
+        '
+        Me.txt2.Location = New System.Drawing.Point(75, 40)
+        Me.txt2.Name = "txt2"
+        Me.txt2.Size = New System.Drawing.Size(134, 20)
+        Me.txt2.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(14, 40)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(49, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Apellidos"
+        '
+        'btn1
+        '
+        Me.btn1.Location = New System.Drawing.Point(215, 13)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(75, 23)
+        Me.btn1.TabIndex = 4
+        Me.btn1.Text = "Agregar"
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'lbox1
+        '
+        Me.lbox1.FormattingEnabled = True
+        Me.lbox1.Location = New System.Drawing.Point(13, 66)
+        Me.lbox1.Name = "lbox1"
+        Me.lbox1.Size = New System.Drawing.Size(277, 95)
+        Me.lbox1.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(305, 176)
+        Me.Controls.Add(Me.lbox1)
+        Me.Controls.Add(Me.btn1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txt2)
+        Me.Controls.Add(Me.txt1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txt1 As TextBox
+    Friend WithEvents txt2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btn1 As Button
+    Friend WithEvents lbox1 As ListBox
 End Class
--- Nombre/Nombre/Form1.vb
+++ Nombre/Nombre/Form1.vb
@@ -1,3 +1,15 @@
 ﻿Public Class Form1
 
+    Dim nombre As String
+    Dim apellido As String
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+
+        lbox1.Items.Add(txt1.Text + " " + txt2.Text)
+
+        txt1.Text = ""
+        txt2.Text = ""
+        txt1.Focus()
+
+    End Sub
+
 End Class
