c6797184074083a2e90b6093af262a7aa24f1020
dulas1 <ti13110192@itess.edu.mx>
2017-08-05 14:22:31 -0500

    Proporciona 2 opciones, eres mayor de 18 o no

--- Opciones/Opciones/Form1.Designer.vb
+++ Opciones/Opciones/Form1.Designer.vb
@@ -22,9 +22,72 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.rbtn2 = New System.Windows.Forms.RadioButton()
+        Me.rbtn1 = New System.Windows.Forms.RadioButton()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btn1)
+        Me.GroupBox1.Controls.Add(Me.rbtn2)
+        Me.GroupBox1.Controls.Add(Me.rbtn1)
+        Me.GroupBox1.Location = New System.Drawing.Point(13, 13)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(110, 101)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Qué edad tienes?"
+        '
+        'btn1
+        '
+        Me.btn1.Location = New System.Drawing.Point(7, 67)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(75, 23)
+        Me.btn1.TabIndex = 2
+        Me.btn1.Text = "Aceptar"
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'rbtn2
+        '
+        Me.rbtn2.AutoSize = True
+        Me.rbtn2.Location = New System.Drawing.Point(7, 44)
+        Me.rbtn2.Name = "rbtn2"
+        Me.rbtn2.Size = New System.Drawing.Size(86, 17)
+        Me.rbtn2.TabIndex = 1
+        Me.rbtn2.TabStop = True
+        Me.rbtn2.Text = "menos de 18"
+        Me.rbtn2.UseVisualStyleBackColor = True
+        '
+        'rbtn1
+        '
+        Me.rbtn1.AutoSize = True
+        Me.rbtn1.Location = New System.Drawing.Point(7, 20)
+        Me.rbtn1.Name = "rbtn1"
+        Me.rbtn1.Size = New System.Drawing.Size(74, 17)
+        Me.rbtn1.TabIndex = 0
+        Me.rbtn1.TabStop = True
+        Me.rbtn1.Text = "mas de 18"
+        Me.rbtn1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(140, 132)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btn1 As Button
+    Friend WithEvents rbtn2 As RadioButton
+    Friend WithEvents rbtn1 As RadioButton
 End Class
--- Opciones/Opciones/Form1.vb
+++ Opciones/Opciones/Form1.vb
@@ -1,3 +1,12 @@
 ﻿Public Class Form1
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
 
+        If rbtn1.Checked Then
+            MessageBox.Show("Tienes mas de 18 años", "Felicidades eres legal")
+
+        ElseIf rbtn2.Checked Then
+            MessageBox.Show("Tienes menos de 18 años", "Aun no eres legal")
+        End If
+
+    End Sub
 End Class
